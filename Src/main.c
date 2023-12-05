fa1d8e5627b0a7a23f15501a5d254a1be4e0d7b8
Victor <90677528+Vernicovskiy@users.noreply.github.com>
2023-12-05 13:09:04 +0300

    Update main.c

--- Src/main.c
+++ Src/main.c
@@ -5,15 +5,13 @@
 #define TIM_EnableIT_UPDATE(TIMx) SET_BIT(TIMx->DIER, TIM_DIER_UIE)
 #define TIM_EnableCounter(TIMx) SET_BIT(TIMx->CR1, TIM_CR1_CEN)
 
-
-	__IO uint32_t Tim10_Count = 0;
-
+__IO uint32_t Tim10_Count = 0;
 
 	void led_init(){
-
+		
 		RCC->AHB1ENR	|= RCC_AHB1ENR_GPIOAEN; //RCC on for GPIO A
-	    GPIOA->MODER &= ~0x00000C00; /* clear pin mode */
-	    GPIOA->MODER |= GPIO_MODER_MODER5_0; /* set pin to output mode */
+		GPIOA->MODER &= ~0x00000C00; /* clear pin mode */
+		GPIOA->MODER |= GPIO_MODER_MODER5_0; /* set pin to output mode */
 
 	}
 
@@ -21,8 +19,7 @@
 		RCC->APB2ENR |= RCC_APB2ENR_TIM10EN; // тактирование
 		NVIC_EnableIRQ(TIM1_UP_TIM10_IRQn); // глоб прер
 		TIM10->PSC = 16000-1;// преддел
-		//CLEAR_REG(TIM10->ARR);
-		TIM10->ARR	= 999;
+		TIM10->ARR = 999;
 	}
 
 	void TIM1_UP_TIM10_IRQHandler(void){
